b06bef5dbb675c23d06ec40f210c171ff62bb87b
TaeJun Kim <pockta14@gmail.com>
2023-11-27 20:01:10 +0900

    사원 리스트 가져오기 수정2
    쿼리문 오류로 쿼리문 수정하니, java.lang.ClassNotFoundException: org.springframework.web.multipart.support.MultipartFilter
    발생

--- HR_PROJECT/src/main/java/attention/mapper/EmployeesMapper.java
+++ HR_PROJECT/src/main/java/attention/mapper/EmployeesMapper.java
@@ -8,9 +8,14 @@ import attention.beans.EmployeesBean;
 
 public interface EmployeesMapper {
 	
-	 @Select("select employee_name, emp.department_name, department_depth, department_depth_row, emp.employee_phone, emp.employee_status "
-	 		+ "from HRPROJECT.employees emp "
-	 		+ "inner join department dept "
-	 		+ "on emp.employee_id = dept.department_head_id")
-	 ArrayList<EmployeesBean> getEmpInfo();
+//	 @Select("select employee_name, emp.department_name, department_depth, department_depth_row, emp.employee_phone, emp.employee_status "
+//	 		+ "from HRPROJECT.employees emp "
+//	 		+ "inner join department dept "
+//	 		+ "on emp.employee_id = dept.department_head_id")
+//	 ArrayList<EmployeesBean> getEmpInfo();
+	 
+	 @Select("select employee_name, department_name, employee_phone, employee_status "
+		 		+ "from HRPROJECT.employees")
+		 ArrayList<EmployeesBean> getEmpInfo();
+	 
 }
