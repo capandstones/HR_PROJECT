fb6ed06c28a190712fe697426786931f02d2bcd3
TaeJun Kim <pockta14@gmail.com>
2023-11-29 11:44:56 +0900

    merge branch all

--- HR_PROJECT/src/main/java/attention/config/ServletAppContext.java
+++ HR_PROJECT/src/main/java/attention/config/ServletAppContext.java
@@ -34,99 +34,99 @@ import attention.mapper.UserMapper;
 @PropertySource("/WEB-INF/properties/db.properties")
 public class ServletAppContext implements WebMvcConfigurer {
 
-   @Value("${db.classname}")
-   private String db_classname;
+	@Value("${db.classname}")
+	private String db_classname;
 
-   @Value("${db.url}")
-   private String db_url;
+	@Value("${db.url}")
+	private String db_url;
 
-   @Value("${db.username}")
-   private String db_username;
+	@Value("${db.username}")
+	private String db_username;
 
-   @Value("${db.password}")
-   private String db_password;
+	@Value("${db.password}")
+	private String db_password;
 
-   @Resource(name = "loginUserBean")
-   private UserBean loginUserBean;
+	@Resource(name = "loginUserBean")
+	private UserBean loginUserBean;
 
-   @Override
-   public void configureViewResolvers(ViewResolverRegistry registry) {
-      // view로 보내질 최종 요청응답에 관한 환경설정
-      WebMvcConfigurer.super.configureViewResolvers(registry);
-      registry.jsp("/WEB-INF/views/", ".jsp");
-   }
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// view로 보내질 최종 요청응답에 관한 환경설정
+		WebMvcConfigurer.super.configureViewResolvers(registry);
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
 
-   // 정적 파일의 경로를 매핑
-   @Override
-   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      // TODO Auto-generated method stub
-      WebMvcConfigurer.super.addResourceHandlers(registry);
-      registry.addResourceHandler("/**").addResourceLocations("/resources/");
-   }
+	// 정적 파일의 경로를 매핑
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
 
-   @Override
-   public void addInterceptors(InterceptorRegistry registry) {
-      WebMvcConfigurer.super.addInterceptors(registry);
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		WebMvcConfigurer.super.addInterceptors(registry);
 
-      CheckLoginInterceptor checkLoginInterceptor = new CheckLoginInterceptor(loginUserBean);
+		CheckLoginInterceptor checkLoginInterceptor = new CheckLoginInterceptor(loginUserBean);
 
-      InterceptorRegistration reg1 = registry.addInterceptor(checkLoginInterceptor);
-      reg1.addPathPatterns("/main/*", "/calendar/*", "/commute/*", "/member/*", "/notice/*", "/user/logout", "/user/modify", "/vacation/*", "/workflow/*");
-      reg1.excludePathPatterns("/user/login");
-   }
+		InterceptorRegistration reg1 = registry.addInterceptor(checkLoginInterceptor);
+		reg1.addPathPatterns("/main/*", "/calendar/*", "/commute/*", "/member/*", "/notice/*", "/user/logout",
+				"/user/modify", "/vacation/*", "/workflow/*");
+		reg1.excludePathPatterns("/user/login");
+	}
 
-   @Bean
-   public BasicDataSource dataSource() {
-      BasicDataSource source = new BasicDataSource();
-      source.setDriverClassName(db_classname);
-      source.setUrl(db_url);
-      source.setUsername(db_username);
-      source.setPassword(db_password);
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource source = new BasicDataSource();
+		source.setDriverClassName(db_classname);
+		source.setUrl(db_url);
+		source.setUsername(db_username);
+		source.setPassword(db_password);
 
-      return source;
-   }
+		return source;
+	}
 
-   // 쿼리문과 접속 정보를 관리하는 객체
-   @Bean
-   public SqlSessionFactory factory(BasicDataSource source) throws Exception {
-      SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-      factoryBean.setDataSource(source);
-      SqlSessionFactory factory = factoryBean.getObject();
-      return factory;
-   }
+	// 쿼리문과 접속 정보를 관리하는 객체
+	@Bean
+	public SqlSessionFactory factory(BasicDataSource source) throws Exception {
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(source);
+		SqlSessionFactory factory = factoryBean.getObject();
+		return factory;
+	}
 
-   @Bean
-   public MapperFactoryBean<UserMapper> getUserMapper(SqlSessionFactory factory) throws Exception {
-      MapperFactoryBean<UserMapper> factoryBean = new MapperFactoryBean<UserMapper>(UserMapper.class);
-      factoryBean.setSqlSessionFactory(factory);
-      return factoryBean;
-   }
-   
-   @Bean
-   public MapperFactoryBean<EmployeesMapper> getEmployeesMapper(SqlSessionFactory factory) throws Exception {
-      MapperFactoryBean<EmployeesMapper> factoryBean = new MapperFactoryBean<EmployeesMapper>(EmployeesMapper.class);
-      factoryBean.setSqlSessionFactory(factory);
-      return factoryBean;
-   }
-	
+	@Bean
+	public MapperFactoryBean<UserMapper> getUserMapper(SqlSessionFactory factory) throws Exception {
+		MapperFactoryBean<UserMapper> factoryBean = new MapperFactoryBean<UserMapper>(UserMapper.class);
+		factoryBean.setSqlSessionFactory(factory);
+		return factoryBean;
+	}
 
-   // ======================================================================
-   @Bean
-   public ReloadableResourceBundleMessageSource messageSource() {
-      ReloadableResourceBundleMessageSource res = new ReloadableResourceBundleMessageSource();
-      res.setBasenames("/WEB-INF/properties/error_message");
-      return res;
-   }
+	@Bean
+	public MapperFactoryBean<EmployeesMapper> getEmployeesMapper(SqlSessionFactory factory) throws Exception {
+		MapperFactoryBean<EmployeesMapper> factoryBean = new MapperFactoryBean<EmployeesMapper>(EmployeesMapper.class);
+		factoryBean.setSqlSessionFactory(factory);
+		return factoryBean;
+	}
 
-   // 소스와 메세지 별도관리하도록 등록
-   @Bean
-   public static PropertySourcesPlaceholderConfigurer PropertySourcesPlaceholderConfigurer() {
-      return new PropertySourcesPlaceholderConfigurer();
-   }
-   
-   @Bean //multipart와 valid를 합꼐 읽 수 있도록 Bean 선언
+	// ======================================================================
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource res = new ReloadableResourceBundleMessageSource();
+		res.setBasenames("/WEB-INF/properties/error_message");
+		return res;
+	}
+
+	// 소스와 메세지 별도관리하도록 등록
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer PropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Bean // multipart와 valid를 합꼐 읽 수 있도록 Bean 선언
 	public StandardServletMultipartResolver multipartResolver() {
-		return new StandardServletMultipartResolver(); //객체 생성하여 반환
+		return new StandardServletMultipartResolver(); // 객체 생성하여 반환
 	}
 
 }
